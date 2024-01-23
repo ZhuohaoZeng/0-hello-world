369d1574136b2bbbcd909fca8a88fae8690a331f
weixiong jiang <weixiongjiang@weixiongs-mbp.wireless-1x.unc.edu>
2024-01-22 19:10:00 -0500

    You

---
+++ .bash_history
@@ -0,0 +1,14 @@
+ls
+exit
+vim hello.c
+
+gcc -Wall hello.c
+./a.out'
+
+vim a.out
+vim a.out
+./a.out
+vim hello.c
+
+
+exit
---
+++ workdir/0-hello-world/hello.c
@@ -0,0 +1,10 @@
+// PID: Zhuohao Zeng
+// I pledge the COMP 211 honor code.
+#include <stdlib.h>
+#include <stdio.h>
+int main() 
+{
+    printf("Hello, world.\n");
+    printf("Welcome to C!\n");
+    return EXIT_SUCCESS;
+}
